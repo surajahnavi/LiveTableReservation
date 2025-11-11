8e1943b6f3e9207358a385a7cde2764861405e2f
suraiahnavil <suraiahnavil@gmail.com>
2025-11-11 23:30:32 +0530

    Fix ESLint error: Remove unused useEffect import

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { API_ENDPOINTS, apiFetch, handleAPIError } from './config/api';
 import RegisterForm from './RegisterForm';
 import LoginForm from './LoginForm';
